5346e29665beb1caa0c30015edcd05b84999e8c3
Kyle Brumm <kjbrumm@gmail.com>
2016-08-16 13:13:55 -0500

    Update js formatting

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-;(function(global) {
+;
+(function(global) {
     /**
      *  Social sharing popup window
      */
     var jsSocialShares = document.querySelectorAll('.js-social-share');
-    if(jsSocialShares) {
+    if (jsSocialShares) {
         [].forEach.call(jsSocialShares, function(anchor) {
             anchor.addEventListener('click', function(e) {
 
@@ -13,7 +14,7 @@
                     left = (screen.width / 2) - (width / 2),
                     top = (screen.height / 2) - (height / 2);
 
-                if(/^(f|ht)tps?:\/\//i.test(url) || /^mailto/i.test(url)) {
+                if (/^(f|ht)tps?:\/\//i.test(url) || /^mailto/i.test(url)) {
                     e.preventDefault();
                     window.open(
                         url,
@@ -27,7 +28,7 @@
     }
 
     // spotifyCurrentlyPlaying.js
-    if(document.querySelector('.scp-container')) {
+    if (document.querySelector('.scp-container')) {
         SCP({
             username: 'kjbrum',
             api_key: 'da3d6b59d72cbd535ce2405ec13b97be'
