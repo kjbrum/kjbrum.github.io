2738e585214bfea5a1d9ccc4df2d0e76bb7f549c
Kyle Brumm <kjbrumm@gmail.com>
2016-08-17 18:48:33 -0500

    Add photography to about page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-;(function(global) {
+;
+(function(global) {
     /**
      *  Social sharing popup window
      */
@@ -55,18 +56,63 @@
 
 
 
-
     /******************************************************
      * Get Dribbble shots for a specific user
      ******************************************************/
-    var getDribbleShots = function(user, token, callback) {
-        // Set the request URL
-        var dribbble_api_url = 'https://api.dribbble.com/v1/users/' + user + '/shots';
+    // var getDribbleShots = function(user, token, callback) {
+    //     // Set the request URL
+    //     var dribbble_api_url = 'https://api.dribbble.com/v1/users/' + user + '/shots';
 
-        // Make a request to the API
+    //     // Make a request to the API
+    //     var request = new XMLHttpRequest();
+    //     request.open('GET', dribbble_api_url, true);
+    //     request.setRequestHeader('Authorization', 'Bearer ' + token);
+
+    //     // Check for a successful response
+    //     request.onload = function() {
+    //         // Parse the response
+    //         var data = JSON.parse(request.responseText);
+
+    //         // Check the status of the request
+    //         if (request.status >= 200 && request.status < 400) {
+    //             // Return the found data
+    //             callback(data);
+    //         } else {
+    //             // Error from the server
+    //             throw data.message;
+    //         }
+    //     };
+
+    //     // Handle any errors
+    //     request.onerror = function() {
+    //         // Connection error
+    //         throw 'connection error';
+    //     };
+
+    //     // Send the request
+    //     request.send();
+    // }
+
+    var requestData = function(options, callback) {
+        // Set the request URL
+        var apiURL;
+        switch (options.service) {
+            case '500px':
+                apiURL = 'https://api.500px.com/v1/photos?consumer_key=' + options.auth + '&feature=user&username=' + options.username + '&image_size=440';
+                break;
+            case 'dribbble':
+                apiURL = 'https://api.dribbble.com/v1/users/' + options.username + '/shots';
+                break;
+        }
+
+        // Make the API request
         var request = new XMLHttpRequest();
-        request.open('GET', dribbble_api_url, true);
-        request.setRequestHeader('Authorization', 'Bearer ' + token);
+        request.open('GET', apiURL, true);
+
+        // Enable additional headers
+        if (options.service == 'dribbble') {
+            request.setRequestHeader('Authorization', 'Bearer ' + options.auth);
+        }
 
         // Check for a successful response
         request.onload = function() {
@@ -93,26 +139,64 @@
         request.send();
     }
 
-    if(document.querySelector('.dribbble-shots')) {
-        getDribbleShots('kjbrum', '305b1f7fb8e23dd9375061f6fa5e26263fdfc0ecbfb50f30379b01f98829f259', function(shots) {
-            if (shots.length > 0) {
+    // Display the Dribbble shots
+    if (document.querySelector('.dribbble-shots')) {
+        requestData({
+            service: 'dribbble',
+            username: 'kjbrum',
+            auth: '305b1f7fb8e23dd9375061f6fa5e26263fdfc0ecbfb50f30379b01f98829f259'
+        }, function(data) {
+            if (data.length > 0) {
                 var list = document.createElement('div');
-                list.className = 'g g-np g-xs-2 g-m-3 shots';
+                list.className = 'g g-s-2 shots';
 
                 // Loop through the shots
-                shots.forEach(function(el, idx, arr) {
-                    var item = document.createElement('div');
-                    item.className = 'gi shot';
+                data.forEach(function(el, idx, arr) {
+                    if (idx < 4) {
+                        var item = document.createElement('div');
+                        item.className = 'gi shot';
 
-                    var shot_html = '<a href="' + el.html_url + '" target="_blank"><img src="' + el.images.normal + '"></a>';
-                    item.innerHTML = shot_html;
+                        var shotHTML = '<a href="' + el.html_url + '" target="_blank"><img src="' + el.images.normal + '"></a>';
+                        item.innerHTML = shotHTML;
 
-                    // Add the new item to the list
-                    list.appendChild(item);
+                        // Add the new item to the list
+                        list.appendChild(item);
+                    }
                 });
 
                 // Add the list to the page
                 document.querySelector('.dribbble-shots').appendChild(list);
+            }
+        });
+    }
+
+    // Display the 500px shots
+    if (document.querySelector('.fivehundred-photos')) {
+        requestData({
+            service: '500px',
+            username: 'kjbrum',
+            auth: 'VoO5pDr8HxVQtrDVeQXKqF6Kzx0PJdlvy0mjNEGt'
+        }, function(data) {
+            if (data.photos.length > 0) {
+                var list = document.createElement('div');
+                list.className = 'g g-s-2 photos';
+
+                // Loop through the photos
+                data.photos.forEach(function(el, idx, arr) {
+                    if (idx < 4) {
+                        var item = document.createElement('div');
+                        item.className = 'gi photo';
+
+                        var photoHTML = '<a href="http://500px.com' + el.url + '" target="_blank"><img src="' + el.image_url + '"></a>';
+                        item.innerHTML = photoHTML;
+
+                        // Add the new item to the list
+                        list.appendChild(item);
+                    }
+                });
+
+                // Add the list to the page
+                document.querySelector('.fivehundred-photos').appendChild(list);
             }
         });
     }
