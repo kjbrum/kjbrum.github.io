7490b5379824b46b245ed655f2e2e468c3226974
Kyle Brumm <kjbrumm@gmail.com>
2018-07-30 21:51:58 -0500

    Cleanup about page and JS

--- js/main.js
+++ js/main.js
@@ -32,12 +32,12 @@
     }
 
     // spotifyCurrentlyPlaying.js
-    if (document.querySelector('.scp-container')) {
-        SCP({
-            username: 'kjbrum',
-            api_key: 'da3d6b59d72cbd535ce2405ec13b97be'
-        });
-    }
+    // if (document.querySelector('.scp-container')) {
+    //     SCP({
+    //         username: 'kjbrum',
+    //         api_key: 'da3d6b59d72cbd535ce2405ec13b97be'
+    //     });
+    // }
 
     // Time to read
     var wordsToMinutes = function(text, imgs) {
@@ -59,51 +59,51 @@
     // sr.reveal('.projects .page-content *');
 
     // Display the Instagram photos
-    if (document.querySelector('.instagram-photos')) {
-        SocialDig({
-            selector: '.instagram-photos',
-            service: 'instagram',
-            user: 'kjbrum',
-            auth: '175436735.49954d7.8ed44a3a4d8c49dd97c4219656c04e29'
-        }, function(data) {
-            if (data.data.length > 0) {
-                var list = document.createElement('div');
-                list.className = 'g g-np g-xs-2 g-s-3 g-l-4 photos';
+    // if (document.querySelector('.instagram-photos')) {
+    //     SocialDig({
+    //         selector: '.instagram-photos',
+    //         service: 'instagram',
+    //         user: 'kjbrum',
+    //         auth: '175436735.49954d7.8ed44a3a4d8c49dd97c4219656c04e29'
+    //     }, function(data) {
+    //         if (data.data.length > 0) {
+    //             var list = document.createElement('div');
+    //             list.className = 'g g-np g-xs-2 g-s-3 g-l-4 photos';
 
-                // Loop through the photos
-                data.data.forEach(function(el, idx, arr) {
-                    if (idx < 12) {
-                        var item = document.createElement('div');
-                        item.className = 'gi photo';
+    //             // Loop through the photos
+    //             data.data.forEach(function(el, idx, arr) {
+    //                 if (idx < 12) {
+    //                     var item = document.createElement('div');
+    //                     item.className = 'gi photo';
 
-                        var caption = '';
-                        if (el.caption) {
-                            caption = '<br>' + el.caption.text;
-                        }
+    //                     var caption = '';
+    //                     if (el.caption) {
+    //                         caption = '<br>' + el.caption.text;
+    //                     }
 
-                        // Embed
-                        // item.innerHTML = '<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style="background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style="background:#F8F8F8; line-height:0; margin-top:40px; padding:50% 0; text-align:center; width:100%;"> <div style="background:url(' + el.images.standard_resolution.url + '); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div> <p style="margin:8px 0 0 0; padding:0 4px;"> <a href="' + el.link + '" style="color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;" target="_blank">' + caption + '</a></p> <p style="color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;">A photo posted by ' + el.user.full_name + ' (@' + el.user.username + ').</p></div></blockquote>';
+    //                     // Embed
+    //                     // item.innerHTML = '<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style="background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style="background:#F8F8F8; line-height:0; margin-top:40px; padding:50% 0; text-align:center; width:100%;"> <div style="background:url(' + el.images.standard_resolution.url + '); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div> <p style="margin:8px 0 0 0; padding:0 4px;"> <a href="' + el.link + '" style="color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;" target="_blank">' + caption + '</a></p> <p style="color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;">A photo posted by ' + el.user.full_name + ' (@' + el.user.username + ').</p></div></blockquote>';
 
-                        // Custom
-                        item.innerHTML = '<a href="' + el.link + '" target="_blank"><div class="img" style="background-image: url(' + el.images.standard_resolution.url + ')"><div class="likes"><div class="text"><i class="fa fa-heart"></i>' + el.likes.count + '</div></div></div></a>';
+    //                     // Custom
+    //                     item.innerHTML = '<a href="' + el.link + '" target="_blank"><div class="img" style="background-image: url(' + el.images.standard_resolution.url + ')"><div class="likes"><div class="text"><i class="fa fa-heart"></i>' + el.likes.count + '</div></div></div></a>';
 
-                        // Add the new item to the list
-                        list.appendChild(item);
-                    }
-                });
+    //                     // Add the new item to the list
+    //                     list.appendChild(item);
+    //                 }
+    //             });
 
-                // Add the list to the page
-                document.querySelector('.instagram-photos').appendChild(list);
+    //             // Add the list to the page
+    //             document.querySelector('.instagram-photos').appendChild(list);
 
-                // Add the embed script
-                var instaScript = document.createElement('script');
-                instaScript.src = '//platform.instagram.com/en_US/embeds.js';
-                instaScript.async = true;
-                instaScript.defer = true;
-                document.getElementsByTagName('body')[0].appendChild(instaScript);
-            }
-        });
-    }
+    //             // Add the embed script
+    //             var instaScript = document.createElement('script');
+    //             instaScript.src = '//platform.instagram.com/en_US/embeds.js';
+    //             instaScript.async = true;
+    //             instaScript.defer = true;
+    //             document.getElementsByTagName('body')[0].appendChild(instaScript);
+    //         }
+    //     });
+    // }
 
     // Oh hello
     console.log("%cOh hello, I'm Kyle :D","background: #514F6F; color: #5AA8B3; line-height: 25px; padding: 5px 10px;")
